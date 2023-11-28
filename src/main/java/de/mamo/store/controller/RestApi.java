8e7389ec185f3246eb17db68e2304883d384758b
marce <marcel.moehn@gmail.com>
2023-11-28 23:36:13 +0100

    working security

--- src/main/java/de/mamo/store/controller/RestApi.java
+++ src/main/java/de/mamo/store/controller/RestApi.java
@@ -3,6 +3,7 @@ package de.mamo.store.controller;
 import de.mamo.store.entities.BookEntity;
 import de.mamo.store.services.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
